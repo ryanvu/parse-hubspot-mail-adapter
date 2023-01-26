7b86a3e4d4c931348fd43d30bae246926808fe55
ryanvu <ryanvu2003@gmail.com>
2023-01-26 10:38:19 -0500

    optional globalization implementation, readme updated

--- README.md
+++ README.md
@@ -3,7 +3,28 @@ Used to send Parse Server password reset and email verification emails though Hu
 
 
 ## How to use
-** Create your emails within your HubSpot dashboard first!
+Create your emails within your HubSpot dashboard first!
+
+In your HubSpot emails for **Email Verification** & **Password Reset** please create properties based off of this **_PublicSingleSendRequestEgg_**:
+```
+{
+  message: { recipient },
+  contactProperties: { firstname },
+  customProperties: { link }, // the link that Parse will create for respective action above
+  emailId: 'your email id for respective email template'
+}
+```
+
+*__Generic Email__*: The body of the generic email will be passed in as a string of markdown.
+```
+{
+  message: { recipient },
+  customProperties: { body },
+  emailId: 'your email id for respective email template'
+}
+```
+
+## Configuration in ParseServer
 
 ```
 const server = ParseServer({
@@ -18,9 +39,42 @@ const server = ParseServer({
       // Your Password Reset email ID
       passwordResetEmailId: 'email-id-for-password-reset-email',
       // Your generic email ID
-      genericEmailId: 'email-id-for-generic-email'
+      genericEmailId: 'email-id-for-generic-email',
+      // This will send the email verification, password reset email with your generic Email Id
+      // true if you want to create your emails with HTML instead of creating them in HubSpot.
+      genericOnly?: boolean,
+      // paths to point to your email html files
+      verificationEmailFolder: './public/path/to/verificationEmails',
+      passwordResetFolder: './public/path/to/passwordResetEmails',
+      // default language if a user does not have a preferred language
+      defaultUserLanguage: 'en',
+      // the name of the column of the user properties for language preferences
+      userLanguagePropertyName: 'lang'
     }
   }
   ...
 });
 ```
+
+## Using genericOnly flag
+If you want to send emails based off of a users language please create a folder with two seperate folders for each kind of Email you'd like to send:
+
+*__example__*
+
+```bash
+public
+├───i18n
+│   ├───passwordReset // en.html, ja.html, ar.html
+│   └───verificationEmail // en.html, ja.html, ar.html
+```
+
+*__HTML Templates__*: 
+**en.html**:
+```html
+<div>
+  <p> Hello <%= firstname %><p>
+  <p> Please verify your email with this <a href="<%= link %>">link</a>.<p>
+</div>
+```
+
+Please write your HTML with the *ejs* template sytax, as this adapter will be looking to replace them.
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const { Client } = require('@hubspot/api-client');
-
+const fs         = require('fs/promises');
 
 const SimpleHubSpotEmailAdapter = hubSpotOptions => {
   if (!hubSpotOptions || !hubSpotOptions.accessToken) {
     throw 'SimpleHubSpotEmailAdapter requires an Access Token';
   }
 
-  const hubspotClient = new Client({ accessToken: hubSpotOptions.accessToken });
+  //Defaults
+  hubSpotOptions.genericOnly         = hubSpotOptions.genericOnly || false;
+  hubSpotOptions.defaultUserLanguage = hubSpotOptions.defaultUserLanguage || 'en';
+
+  const hubspotClient     = new Client({ accessToken: hubSpotOptions.accessToken });
+
+  const templateLink      = '<%= link %>';
+  const templateFirstName = '<%= firstname %>';
   
   const getRecipient = (user) => {
     return user.get('email');
@@ -17,32 +24,67 @@ const SimpleHubSpotEmailAdapter = hubSpotOptions => {
       const apiResponse = await hubspotClient.marketing.transactional.singleSendApi.sendEmail(options);
       return apiResponse;
     } catch (error) {
+      console.log(error);
       return error;
     }
   };
 
   const sendVerificationEmail = async (options) => {
-    const to        = getRecipient(options.user);
-    const firstname = options.user.get('firstName') || '';
-    const link      = options.link;
-    return await sendEmail({
+    const to         = getRecipient(options.user);
+    const firstname  = options.user.get('firstName') || '';
+    const link       = options.link;
+    let emailOptions = {
       message:           { to },
       contactProperties: { firstname },
       customProperties:  { link },
-      emailId:           hubSpotOptions.verificationEmailId
-    });
+      emailId:           parseInt(hubSpotOptions.verificationEmailId, 10)
+    };
+    
+    const genericOnly = hubSpotOptions.genericOnly;
+    if (genericOnly) {
+      const locale    = options.user.get(hubSpotOptions.userLanguagePropertyName) || hubSpotOptions.defaultUserLanguage;
+      
+      const email   = await fs.readFile(`${hubSpotOptions.verificationEmailFolder}/${locale}.html`, { encoding: 'utf-8' });
+      const subject = 'Email Verification';
+      const body    = email.replaceAll(templateLink, link).replaceAll(templateFirstName, firstname);
+  
+      emailOptions = {
+        message:           { to },
+        customProperties:  { body, subject },
+        emailId:           parseInt(hubSpotOptions.genericEmailId)
+      };
+    }
+  
+    return await sendEmail(emailOptions);
   };
 
   const sendPasswordResetEmail = async (options) => {
     const to        = getRecipient(options.user);
     const firstname = options.user.get('firstName') || '';
     const link      = options.link;
-    return await sendEmail({
+    let emailOptions = {
       message:           { to },
       contactProperties: { firstname },
       customProperties:  { link },
-      emailId:           hubSpotOptions.passwordResetEmailId
-    });
+      emailId:           parseInt(hubSpotOptions.passwordResetEmailId, 10)
+    };
+    
+    const genericOnly = hubSpotOptions.genericOnly;
+    if (genericOnly) {
+      const locale    = options.user.get(hubSpotOptions.userLanguagePropertyName) || hubSpotOptions.defaultUserLanguage;
+
+      const email   = await fs.readFile(`${hubSpotOptions.passwordResetFolder}/${locale}.html`, { encoding: 'utf-8' });
+      const subject = 'Reset your password';
+      const body    = email.replaceAll(templateLink, link).replaceAll(templateFirstName, firstname);
+
+      emailOptions = {
+        message:           { to },
+        customProperties:  { body, subject },
+        emailId:           parseInt(hubSpotOptions.genericEmailId)
+      };
+    }
+
+    return await sendEmail(emailOptions);
   };
 
   const sendMail = async (mail) => {
@@ -52,7 +94,7 @@ const SimpleHubSpotEmailAdapter = hubSpotOptions => {
     return await sendEmail({
       message:          { to },
       customProperties: { body },
-      emailId:          hubSpotOptions.genericEmailId
+      emailId:          parseInt(hubSpotOptions.genericEmailId, 10)
     });
   };
 
